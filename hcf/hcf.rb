f086725737c47e3a7e08b21c3d92da6a7ca4382e
Railsfactory-SurekaPriya <rathnamsureka@gmail.com>
2014-07-03 12:58:01 +0530

    Finding gcd and lcm

--- hcf/hcf.rb
+++ hcf/hcf.rb
@@ -1,21 +1,19 @@
-puts"enter the two numbers"
-a=gets.chomp
-a=a.to_i
-b=gets.chomp
-b=b.to_i
-a1 = a
-b1 = b
+puts"Enter two integers"
+	a=gets.chomp
+	a=a.to_i
+	b=gets.chomp
+	b=b.to_i
+	a1 = a
+	b1 = b
 
-while (b != 0) do
-    t = b
-    b = a % b
-    a = t
+while (b1 != 0) do
+    t = b1
+    b1 = a1 % b1
+    a1 = t
 end
-gcd = a
-lcm = (a1 * b1) / gcd
-
- 
- puts ("Greatest common divisor  = #{gcd}")
- puts ("Least common multiple = #{lcm}")
+	gcd = a1
+	lcm = (a * b) / gcd
+	puts ("Greatest common divisor of #{a} and #{b} = #{gcd}")
+	puts ("Least common multiple of #{a} and #{b} = #{lcm}")
  
   
