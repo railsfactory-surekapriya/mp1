d23034d79035864c6950f1514739a52a4ddedc9b
surekapriya <surekapriya@railsfactory.org>
2014-07-04 10:33:26 +0530

    print pattern program

---
+++ pattern/pattern.rb
@@ -0,0 +1,13 @@
+puts "Enter the number of rows in pyramid of stars you wish to see "
+n=gets.chomp.to_i
+temp = n
+for row in 1..n
+  for c in 1..temp
+    printf " "
+  end
+  temp = temp - 1
+    for i in 1..(2 * row-1)
+      printf "*"
+    end
+   printf "\n"
+end
